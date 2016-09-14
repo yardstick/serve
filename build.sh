db2b707b445d2a67e4b8fc1146d1bf0148fcacf3
Daniel Huckstep <darkhelmet@darkhelmetlive.com>
2016-09-14 17:59:46 -0300

    just docker things

--- build.sh
+++ build.sh
@@ -2,5 +2,7 @@
 set -e
 rm -f serve
 docker run --rm -it -v "$PWD":/usr/src/app -w /usr/src/app golang:alpine ./compile.sh
-docker build -t darkhelmetlive/serve .
-docker push darkhelmetlive/serve
+tag=`git rev-parse --short HEAD`
+docker build -t yardstick/serve:latest -t yardstick/serve:$tag .
+docker push yardstick/serve:latest
+docker push yardstick/serve:$tag
